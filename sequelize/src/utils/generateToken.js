5fd9ce8a7b683658e231bafbf19de6fc01e7d59e
sandeep kumar murmu <sandeep.16murmu@gmail.com>
2022-08-08 01:41:25 +0530

    token generator

---
+++ sequelize/src/utils/generateToken.js
@@ -0,0 +1,10 @@
+// importing from library
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+// token key
+const tokenKey = process.env.TOKEN_KEY;
+
+module.exports = (data) => {
+  return jwt.sign(data, tokenKey, { expiresIn: 60*10 });
+};
