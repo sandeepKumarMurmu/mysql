d22a205a28a522ae7c88644776017a3abae5e788
sandeep kumar murmu <sandeep.16murmu@gmail.com>
2022-08-08 01:40:40 +0530

    delete one

---
+++ sequelize/src/controller/deleteController/deleteOne.js
@@ -0,0 +1,24 @@
+// importing User model
+const { User } = require("../../model/userMode/UserModel");
+
+// create user function
+const deleteOne = async (req, res) => {
+  const { id } = req.params;
+  console.log(typeof id)
+  
+  try {
+    await User.destroy({ where: { id } });
+    return res
+      .status(200)
+      .json({ message: "deleted successfully", data: [], status: true });
+  } catch (e) {
+    return res.status(400).json({
+      data: "",
+      error: e,
+      hint: "error in find one",
+      status: false,
+    });
+  }
+};
+
+module.exports = deleteOne;
