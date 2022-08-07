92de80ee3a0bec139aa841468f1ca51eac95087e
sandeep kumar murmu <sandeep.16murmu@gmail.com>
2022-08-08 01:43:43 +0530

    token ferifyer

---
+++ sequelize/src/middleware/validatin/varifyToken.js
@@ -0,0 +1,25 @@
+// importing from package
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const tokenKey = process.env.TOKEN_KEY;
+
+const tokenVerification = async (req, res, next) => {
+  const { token } = req.headers;
+
+  // console.log(token)
+  try {
+    jwt.verify(token, tokenKey);
+    next();
+  } catch (e) {
+    return res.status(400).json({
+      data: "",
+      message: "please login and try again latter",
+      hint: e.message,
+      status: false,
+    });
+  }
+};
+
+// exporting validation
+module.exports = tokenVerification;
