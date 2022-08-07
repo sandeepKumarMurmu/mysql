723a8cb559a07697b5786eca2b44209893b1fb91
sandeep kumar murmu <sandeep.16murmu@gmail.com>
2022-08-08 01:45:01 +0530

    few more updates

---
+++ sequelize/src/model/userMode/UserModel.js
@@ -0,0 +1,27 @@
+// import from library
+const { DataTypes } = require("sequelize");
+
+// importing database
+const db = require("../../config/Connection");
+
+// creating user model
+const User = db.define(
+  "user",
+  {
+    token: { type: DataTypes.STRING },
+    firstName: { type: DataTypes.STRING, allowNull: false, trim: true },
+    lastName: { type: DataTypes.STRING, allowNull: false, trim: true },
+    middleName: { type: DataTypes.STRING, allowNull: true, trim: true },
+    gender: { type: DataTypes.STRING, allowNull: false, trim: true },
+    dateOfBirth: { type: DataTypes.DATE, allowNull: false },
+    email: { type: DataTypes.STRING, allowNull: false, trim: true },
+    password: { type: DataTypes.STRING, allowNull: false },
+  },
+  { timestamps: true }
+);
+
+// synchronisig with databse
+db.sync();
+
+// exporting model
+module.exports = { User };
