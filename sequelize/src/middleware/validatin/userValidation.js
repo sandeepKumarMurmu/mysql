d622775379bc30263d3ca862452fc64a3ca35b62
sandeep kumar murmu <sandeep.16murmu@gmail.com>
2022-08-08 01:43:23 +0530

    user validation

---
+++ sequelize/src/middleware/validatin/userValidation.js
@@ -0,0 +1,38 @@
+// importing from library
+const { validationResult } = require("express-validator");
+
+// importing user model
+const { User } = require("../../model/userMode/UserModel");
+
+const userValidation = async (req, res, next) => {
+  const { email } = req.body;
+  let errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res
+      .status(400)
+      .json({ message: errors.errors[0].msg, data: [], status: false });
+  }
+  try {
+    const oldUser = await User.findOne({ where: { email } });
+
+    if (oldUser)
+      return res.status(400).json({
+        message: "user already exist please try another email",
+        data: [],
+        status: false,
+      });
+
+    next();
+  } catch (e) {
+    return res.status(400).json({
+      data: "",
+      error: e,
+      hint: "error in login validation",
+      status: false,
+    });
+  }
+};
+
+// exporting validation
+module.exports = userValidation;
