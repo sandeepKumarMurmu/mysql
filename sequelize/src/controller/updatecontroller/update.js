fa29523161e10df20108bdd8e19b8f35216ece33
sandeep kumar murmu <sandeep.16murmu@gmail.com>
2022-08-08 01:42:30 +0530

    update

---
+++ sequelize/src/controller/updatecontroller/update.js
@@ -0,0 +1,23 @@
+// importing User model
+const { User } = require("../../model/userMode/UserModel");
+
+// create user function
+const updatedOne = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await User.update(req.body, { where: { id } });
+    return res
+      .status(200)
+      .json({ message: "update successfull", data: "", status: true });
+  } catch (e) {
+    return res.status(400).json({
+      data: "",
+      error: e,
+      hint: "error in update one",
+      status: false,
+    });
+  }
+};
+
+module.exports = updatedOne;
