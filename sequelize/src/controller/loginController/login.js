9527f5068facf0c61962be3ca815484ccd618a7b
sandeep kumar murmu <sandeep.16murmu@gmail.com>
2022-08-08 01:42:07 +0530

    login done

---
+++ sequelize/src/controller/loginController/login.js
@@ -0,0 +1,21 @@
+// importing token generator
+const TokenGenerator = require("../../utils/generateToken");
+
+// create user function
+const LoginUser = async (req, res) => {
+  try {
+    const token = TokenGenerator(req.body);
+    return res
+      .status(200)
+      .json({ message: "login success", data: token, status: true });
+  } catch (e) {
+    return res.status(400).json({
+      data: "",
+      error: e,
+      hint: "error in createController",
+      status: false,
+    });
+  }
+};
+
+module.exports = LoginUser;
