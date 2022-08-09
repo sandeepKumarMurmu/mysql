aed7249c16c87867c303f237a1ca49b492a39be0
sandeep kumar murmu <sandeep.16murmu@gmail.com>
2022-08-09 08:16:31 +0530

    changes

--- sequelize/index.js
+++ sequelize/index.js
@@ -3,11 +3,15 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-// importing connection 
+// importing connection
 const connection = require("./src/config/Connection");
 
 // importing endpoints
-const EndPoints=require("./src/routes/userRoute")
+// const EndPoints=require("./src/routes/userRoute")
+const PostEndPoints = require("./src/routes/postendpoints/user");
+const UpdateEndPoints = require("./src/routes/updateendpoints/user");
+const DeleteEndPoints = require("./src/routes/deleteendpoints/user");
+const GetEndPoints = require("./src/routes/getendpoints/user");
 
 // initializing app
 const app = express();
@@ -16,7 +20,11 @@ app.use(express.json());
 app.use(cors());
 
 // routes
-app.use("/user/api/v1",EndPoints );
+// users api end points
+app.use("/api/v1", PostEndPoints);
+app.use("/api/v1", UpdateEndPoints);
+app.use("/api/v1", DeleteEndPoints);
+app.use("/api/v1", GetEndPoints);
 
 // delcalring port
 const port = process.env.PORT || 8080;
