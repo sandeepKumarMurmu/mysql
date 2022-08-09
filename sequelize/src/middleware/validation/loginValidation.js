87905947b9c2c091753a09ae09758b1688885ec8
sandeep kumar murmu <sandeep.16murmu@gmail.com>
2022-08-09 17:19:31 +0530

    no change

--- sequelize/src/middleware/validation/loginValidation.js
+++ sequelize/src/middleware/validation/loginValidation.js
@@ -8,6 +8,7 @@ const { User } = require("../../model/userModel/UserModel");
 const compare = require("../../utils/functions/comparePassword");
 
 const loginValidation = async (req, res, next) => {
+  console.log(req.body);
   let errors = validationResult(req);
   const { email, password } = req.body;
 
@@ -18,13 +19,13 @@ const loginValidation = async (req, res, next) => {
   }
   try {
     const oldUser = await User.findOne({ where: { email } });
+
     if (!oldUser)
       return res.status(400).json({
         message: "email or password is not correct",
         data: [],
         status: false,
       });
-
     if (!compare(password, oldUser.password))
       return res.status(400).json({
         message: "email or password is not correct",
