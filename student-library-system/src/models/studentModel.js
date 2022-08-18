6fa561944df151bedeb8ffdb2a315be13624fa04
sandeep kumar murmu <sandeep.16murmu@gmail.com>
2022-08-19 00:46:35 +0530

    all the models are read

---
+++ student-library-system/src/models/studentModel.js
@@ -0,0 +1,28 @@
+// connection file
+const connection = require("../config/connection");
+
+// importing from library
+const { DataTypes } = require("sequelize");
+
+// creating and exporting student model
+module.exports = connection.define(
+  "student",
+  {
+    studentId: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    studentFullName: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+    },
+    studentEmail: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+      unique: true,
+    },
+    studentAddress: { type: DataTypes.STRING(100) },
+  },
+  { timestamps: true }
+);
---
+++ student-library-system/src/models/yearModel.js
@@ -0,0 +1,22 @@
+// connection file
+const connection = require("../config/connection");
+
+// importing from library
+const { DataTypes } = require("sequelize");
+
+// creating and exporting year model
+module.exports = connection.define(
+  "year",
+  {
+    yearId: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    yearName: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  { timestamps: true }
+);
